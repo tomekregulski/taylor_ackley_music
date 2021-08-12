62954926871451a87e5a13b1f66b358c25463572
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-08-11 21:58:19 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>This is a test for Taylor Ackley's website</h1>
+    </>
+  );
+}
+
+export default App;
