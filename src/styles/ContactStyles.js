f6b296ff0be5421366d905ee60ab18ca5abf72f1
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-08-15 14:33:13 -0400

    Emails now include all content

--- src/styles/ContactStyles.js
+++ src/styles/ContactStyles.js
@@ -1,19 +1,45 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: {
-    textAlign: 'center',
-    fontSize: '48px',
-  },
-  input: {
-    width: '600px',
-  },
-  textarea: {
-    height: '10em',
-    width: '600px',
-  },
-  contactContainer: {
+  root: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '4rem',
+    padding: 'theme.spacing(2)',
+
+    '& .MuiTextField-root': {
+      margin: 'theme.spacing(1)',
+      width: '300px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: 'theme.spacing(2)',
+    },
+  },
+  heading: {
+    marginTop: '6rem',
+    textAlign: 'center',
+  },
+  submitBtn: {
+    animation: '$moveInBottom 5000ms',
+    background: 'transparent',
+    border: '2px solid #17D0FF',
+    color: 'black',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontWeight: '400',
+    transition: '.7s',
+    '&:hover': {
+      transform: 'translateY(-3px)',
+      background: 'transparent',
+      boxShadow: '0 0.3rem 0.2rem rgba(0, 0, 0, 0.256)',
+      transition: '.7s',
+    },
+    '&active': {
+      transform: 'translateY(-1px)',
+      boxShadow: '0 0.2rem 0.1rem rgba(0, 0, 0, 0.256)',
+      transition: '0.4s',
+    },
   },
 };
